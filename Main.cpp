60c71740e504ae5692efcc2a8d52a6ee6c285642
Jonathan-0207 <100753984+Jonathan-0207@users.noreply.github.com>
2022-05-21 23:34:04 +0930

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include "Game.h"
+
+int main(){
+
+	Game game;
+	game.gameLoop(game);
+
+
+	return 0;
+}
---
+++ objects.h
@@ -0,0 +1,33 @@
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+class objects
+{
+    protected:
+        Vector2f position;
+        int mass;
+        Vector2f velocity;
+        CircleShape shape;
+
+    public:
+
+        // abstract function get mass
+        virtual int get_mass() = 0;
+
+        // abstract function set position
+        //virtual void set_position() = 0;
+
+        // abstract fucntion get position
+        virtual Vector2f get_position() = 0;
+
+        virtual void render(sf::RenderWindow* win) = 0;
+
+};
+
+
+#endif
